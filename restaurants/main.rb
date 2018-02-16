7c80c8cf7eb84c01991b76b660d687eb16d00f70
Laven Morrow <lmorrow13@live.com>
2018-02-16 15:53:59 -0700

    initail build on main

--- restaurants/main.rb
+++ restaurants/main.rb
@@ -12,4 +12,22 @@ get '/about' do
 	erb :about
 end 
 
-get '/'
+get '/restaurant_profile' do
+	erb :restaurant_profile
+end
+
+get '/restaurants' do
+	erb :restaurants
+end
+
+get '/sign_in' do
+	erb :sign_in
+end
+
+get '/sign_up' do
+	erb :sign_up
+end
+
+get '/user_profile' do
+	erb :user_profile
+end
